a623fae161ba8ea664fb70e9336c3ab0c8d08417
Tunji Akanbi <you@example.com>
2021-04-27 11:47:16 -0500

    gsap now works, still needs fixing

--- src/Test.js
+++ src/Test.js
@@ -1,11 +1,13 @@
 import {useEffect, useRef, useState} from "react";
 import {textIntro} from "./Intro";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import './Test.css';
 function Test() {
-    let intro = useRef(null);
+    // let intro = useRef(null);
     const [music, setMusic] = useState([]);
     useEffect(() => {
-        textIntro(intro);
+        // textIntro(intro);
         const getData = () => {
             fetch("data.json", {
                     headers: {
@@ -15,8 +17,6 @@ function Test() {
             })
             .then((response) => response.json())
             .then((data) => setMusic(data));
-           
-        
         };
       
 getData();
@@ -25,8 +25,9 @@ setTimeout(()=>  {
 }, 1000);
 
 function animate() {
-    
+    gsap.registerPlugin(ScrollTrigger);
     const tl = new gsap.timeline();
+    tl.from('#test', {autoAlpha: 0})
     tl.from('.intro', {
         xPercent: -20,
         opacity: 0,
@@ -35,6 +36,37 @@ function animate() {
         scale: -1,
         ease: "back",
       });
+
+
+
+
+    tl.from('.m_Artist', {
+        // tl2.from(sect3, {
+        // duration: 1,
+        autoAlpha: 0,
+        x: 100,
+        stagger: 0.2,
+        ease: "linear",
+        scrollTrigger: {
+            trigger: '.m_Artist',
+            // markers: true,
+            markers: {
+                startColor: "purple",
+                endColor: "fuchsia",
+                fontSize: "1em",
+                indent: 100,
+            },
+            //start: "top bottom",//first value is start, second value is scroller-start, same for end
+             start: "top 60%",
+            // end: "bottom 90%",
+            // end: () => `+=${document.querySelector('.sect3').offsetHeight}`,
+            end: "top 40%",
+            //  pin: true,
+            toggleActions: "play none reverse reset",
+            scrub: true,
+        }
+    })
+          
     tl.from('.artists', {
         yPosition: -50,
         opacity: 0, 
@@ -43,6 +75,8 @@ function animate() {
         // repeat: -1,
         ease: "back"
     });
+
+    
 };
 
 // textIntro(intro);
@@ -50,18 +84,18 @@ function animate() {
 
 
     return (
-        <div>
-            <h1 className ="intro" ref={(el) => (intro = el)}>My Awesome Music App!</h1>
-            {/* <h1 className ="intro">My Awesome Music App!</h1> */}
+        <div id="test">
+            {/* <h1 className ="intro" ref={(el) => (intro = el)}>My Awesome Music App!</h1> */}
+            <h1 className ="intro">My Awesome Music App!</h1>
            {music.map((m) => (
-               <div key={m.id}>
+               <div key={m.id} className="m_Artist">
                <h2 className="artists">Artist: {m.artistname}</h2>
                <h2 className="artists">Album: {m.album}</h2>
                <h2 className="artists">Record Label: {m.label}</h2>
                <h2 className="artists">Release Year: {m.year}</h2>
                <img className="artists" src={m.albumImg} alt={m.artistname}/>
                <h4 className="artists">Artist Bio:</h4>
-                <p className="artists">{m.bio}</p>
+                {/* <p className="artists">{m.bio}</p> */}
                </div>
            ))} 
         </div>
